52397ece2f33cc7e71f7bbf6ff016d68bdceb8fe
Irina <andrey_k.90@mail.ru>
2023-11-08 07:46:51 +0300

    Commit all

---
+++ CalculateInterface.java
@@ -0,0 +1,14 @@
+/*
+CalculateInterface интерфейс для реализации методов;
+ */
+public interface CalculateInterface {
+    Calculator additionNumbers(Calculator numberNew);
+
+    Calculator subtractNumbers(Calculator numberNew);
+
+    Calculator multiplyNumbers(Calculator numberNew);
+
+    Calculator divideNumbers(Calculator numberNew);
+
+    String toString();
+}
